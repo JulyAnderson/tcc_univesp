6abff0cb1d170b33e6fa8aaf6a0f99c11e033bcc
July Anderson <and.lou19@gmail.com>
2024-10-15 19:22:19 -0300

    Resolvendo conflitos de merge

--- data_process.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd 
-
-df1 = pd.read_csv('dados_coletados_PNCP_ate_pagina_74_normalize.csv')
-df2 = pd.read_csv('dados_coletados_PNCP_ate_pagina_74.csv')
-
-print(df1.columns)
-print(df2.columns)
